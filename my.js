d66e3675c298746fe934c58658f2af6542b71737
alorobino <58748711+alorobino@users.noreply.github.com>
2022-02-07 22:41:20 +0100

    Update my.js

--- my.js
+++ my.js
@@ -15,3 +15,14 @@ const cycleText = () => {
 };
 cycleText();
 setInterval(cycleText, 1000);
+
+const textList2 = ["Corgi", "Shih Tzu", "Pug", "Dachshund"];
+const cycle2 = document.querySelector("#cycle2");
+let i = 0;
+const cycleText2 = () => {
+  cycle2.innerHTML = textList2[i];
+  i = ++i % textList2.length;
+};
+cycleText2();
+setInterval(cycleText2, 2000);
+
