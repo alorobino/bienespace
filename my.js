8a4f601619964b1d9a429df46979ae0f7ab2a4b2
alorobino <58748711+alorobino@users.noreply.github.com>
2022-02-07 22:05:41 +0100

    Update my.js

--- my.js
+++ my.js
@@ -6,3 +6,11 @@ $('.texts').textillate({
     loop: true
 });
 });
+const cycle = document.querySelector("#cycle");
+let i = 0;
+const cycleText = () => {
+  cycle.innerHTML = textList[i];
+  i = ++i % textList.length;
+};
+cycleText();
+setInterval(cycleText, 1000);
