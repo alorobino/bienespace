5b93d6278b4c7a8b594ab5526051c6ced7f4f032
alorobino <58748711+alorobino@users.noreply.github.com>
2022-02-07 20:01:28 +0100

    Create my.js

---
+++ my.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.texts').textillate({
+    minDisplayTime: 1000, 
+    in: { effect: 'flipInX', sync: true }, 
+    out :{  delay: 3, effect: 'lightSpeedOut', sync: true},
+    loop: true
+});
+});
