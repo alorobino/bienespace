83d54be92c81b13032ea9bb63f68eb2ba49ef465
alorobino <58748711+alorobino@users.noreply.github.com>
2022-02-08 16:24:18 +0100

    Add files via upload

--- my.js
+++ my.js
@@ -12,9 +12,25 @@ const textList2 = ["time", "freedom", "money", "calm", "happiness", "rest", "goo
 const cycle2 = document.querySelector("#cycle2");
 let j = 0;
 const cycleText2 = () => {
-  cycle2.innerHTML = textList2[i];
+  cycle2.innerHTML = textList2[j];
   j = ++j % textList2.length;
 };
 cycleText2();
 setInterval(cycleText2, 800);
 
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height= "10%";
+    document.getElementById("lgo").style.height = "100%";
+
+  } else {
+    document.getElementById("header").style.height = "20%";
+    document.getElementById("lgo").style.height = "100%";
+
+  }
+}
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
